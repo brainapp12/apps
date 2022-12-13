f7c0a39ee960cb653e2dab9d912278feff15db2d
brainapp12 <120470131+brainapp12@users.noreply.github.com>
2022-12-14 01:55:08 +0900

    Update talentum_tpa.py

--- talentum_tpa.py
+++ talentum_tpa.py
@@ -163,6 +163,7 @@ if talent_search :
   df_talent = df_tweet.rename(columns={'username':'ユーザーID','text':'ツイート本文','description':'プロフィール','url':'ツイートのURL'})
   df_talent = df_talent.groupby('ツイート本文',as_index=False).head(1)
   df_talent = df_talent[df_talent['ツイート本文'].str.contains('企業|事業|面接|採用|二卒|第二新卒|就活|転職|勉強|働き方|就職|同期|卒|焦り')]
+  df_talent = df_talent[~df_talent['ツイート本文'].str.contains('クス|中国人|RT|エロ')]
 df_talent
 
 
