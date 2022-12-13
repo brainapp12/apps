726b6056c0b85e5e072f951e100b3557e2452b49
brainapp12 <120470131+brainapp12@users.noreply.github.com>
2022-12-14 00:37:57 +0900

    Update talentum_tpa.py

--- talentum_tpa.py
+++ talentum_tpa.py
@@ -164,7 +164,8 @@ df_talent
 
 profile_search = st.button("Search Profile")
 if pforile_search :
-  df_profile = df_talent[df_talent['description'].str.contains(profile_search)]
+  profile_keyword = st.text_input('対象人材のキーワードを入力してください')  
+  df_profile = df_talent[df_talent['description'].str.contains(profile_keyword)]
 df_profile
 
 
